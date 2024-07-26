7deec651bd61f3db08ce89b89904c47818f51e8c
Sadia Asif Khan <sadia.rubab1988@gmail.com>
2024-07-26 22:02:40 +0500

    Update calculator.ts

--- calculator.ts
+++ calculator.ts
@@ -3,7 +3,7 @@ console.log('Calculator')
 let num1 = Number(prompt('Enter first number: '))
 let num2 = Number(prompt('Enter second number: '))
 let op = prompt('Enter operator: ')
-// created by Najaf Ali
+// created by Sadia Asif Khan
 if (op=='+'){
     console.log(num1+num2)
 }
