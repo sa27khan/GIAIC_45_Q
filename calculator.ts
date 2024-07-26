642d221f66ca25a5a4a34a98d177ea73a92e5f19
Sadia Asif Khan <sadia.rubab1988@gmail.com>
2024-07-26 17:54:43 +0500

    Add files via upload

---
+++ calculator.ts
@@ -0,0 +1,18 @@
+console.log('Calculator')
+
+let num1 = Number(prompt('Enter first number: '))
+let num2 = Number(prompt('Enter second number: '))
+let op = prompt('Enter operator: ')
+// created by Najaf Ali
+if (op=='+'){
+    console.log(num1+num2)
+}
+if (op=='-'){
+    console.log(num1-num2)
+}
+if (op=='*'){
+    console.log(num1*num2)
+}
+if (op=='/'){
+    console.log(num1/num2)
+}
